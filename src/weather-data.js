539d2f95c110bfce94e0995eed17ca5d75048b45
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-04-11 13:31:57 +0800

    add more weather data properties

--- src/weather-data.js
+++ src/weather-data.js
@@ -1,13 +1,26 @@
 // Create class that represents weather data
 export default class WeatherData {
-  // properties:
-  //  datetime, conditions, humidity, windspeed, temp, days(daily data)
-  constructor({ datetime, conditions, humidity, windspeed, temp, days = [] }) {
+  constructor({
+    datetime,
+    conditions,
+    icon,
+    temp,
+    tempmin,
+    tempmax,
+    feelslike,
+    humidity,
+    windspeed,
+    pressure,
+  }) {
     this.datetime = datetime;
     this.conditions = conditions;
+    this.icon = icon;
+    this.temp = temp;
+    this.tempmin = tempmin;
+    this.tempmax = tempmax;
+    this.feelslike = feelslike;
     this.humidity = humidity;
     this.windspeed = windspeed;
-    this.temp = temp;
-    this.days = days;
+    this.pressure = pressure;
   }
 }
